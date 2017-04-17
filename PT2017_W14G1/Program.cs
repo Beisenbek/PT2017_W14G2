80fca58b34dd6158afcf812e177e53df9fc9f952
beisenbek.baisakov <beisenbek.baisakov@yandex.ru>
2017-04-17 16:34:42 +0600

    init

--- PT2017_W14G1/Program.cs
+++ PT2017_W14G1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,156 @@ namespace PT2017_W14G1
     class Program
     {
         static void Main(string[] args)
+        {
+
+            while (true)
+            {
+
+                string dbname = Console.ReadLine();
+                string tablename = Console.ReadLine();
+
+                DBConnect dbc = new DBConnect(dbname, tablename);
+
+
+                List<string>[] x = dbc.Select();
+
+                for (int i = 0; i < x[0].Count; ++i)
+                {
+                    Console.Write(x[0][i] + " " + x[1][i] + " " + x[2][i] + "\n");
+                }
+            }
+              
+            
+        }
+    }
+
+    class DBConnect
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string table;
+        private string uid;
+        private string password;
+
+        //Constructor
+        public DBConnect(string dbname, string tablename)
+        {
+            server = "localhost";
+            database = dbname;
+            table = tablename;
+            uid = "root";
+            password = "";
+            string connectionString = string.Format("SERVER={0};DATABASE={1};UID={2};PASSWORD={3};", server, database, uid, password);
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+        //open connection to database
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                //When handling errors, you can your application's response based 
+                //on the error number.
+                //The two most common error numbers when connecting are as follows:
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+                switch (ex.Number)
+                {
+                    case 0:
+                        Console.WriteLine("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        Console.Write("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        //Close connection
+        private bool CloseConnection()
+        {
+            return false;
+        }
+
+        //Insert statement
+        public void Insert()
+        {
+        }
+
+        //Update statement
+        public void Update()
+        {
+        }
+
+        //Delete statement
+        public void Delete()
+        {
+        }
+
+        //Select statement
+        public List<string>[] Select()
+        {
+            string query = "SELECT * FROM " + table;
+
+            //Create a list to store the result
+            List<string>[] list = new List<string>[3];
+            list[0] = new List<string>();
+            list[1] = new List<string>();
+            list[2] = new List<string>();
+
+            //Open connection
+            if (this.OpenConnection() == true)
+            {
+                //Create Command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                //Create a data reader and Execute the command
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                //Read the data and store them in the list
+                while (dataReader.Read())
+                {
+                    list[0].Add(dataReader[0] + "");
+                    list[1].Add(dataReader[1] + "");
+                    list[2].Add(dataReader[2] + "");
+                }
+
+                //close Data Reader
+                dataReader.Close();
+
+                //close Connection
+                this.CloseConnection();
+
+                //return list to be displayed
+                return list;
+            }
+            else
+            {
+                return list;
+            }
+        }
+
+        //Count statement
+        public int Count()
+        {
+            return 0;
+        }
+
+        //Backup
+        public void Backup()
+        {
+        }
+
+        //Restore
+        public void Restore()
         {
         }
     }
